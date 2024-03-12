c8dd0b63c68eb194b452b1e707cd41c05fddc418
santinobravo <bravosantino@gmail.com>
2024-03-12 15:52:28 -0300

    primer desafio

--- testing.js
+++ testing.js
@@ -2,7 +2,7 @@ const Productmanager = require("./productManager");
 
 const producto = new Productmanager()
 
-// console.log(producto.getProductsById(1));
+
 console.log(producto.addProduct('Camiseta fútbol','Belgrano Titular 2024', 45000,'www.google.com','98344', 5));
 console.log(producto.addProduct('Camiseta fútbol','Belgrano Suplente 2024', 45000,'www.google.com','98345', 3));
 
